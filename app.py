39e43526d00ccf19f46243b79861adb5efff82ff
qhuydtvt <qhuydtvt@gmail.com>
2016-10-20 02:51:08 +0700

    Added database with GET posts

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import *
+from mlab import *
+from mongoengine import *
 
 app = Flask(__name__)
+mlab_connect()
 
+class Post(Document):
+    title = StringField()
+    content = StringField()
+    def to_json(self):
+        return {"title": self.title, "content":self.content}
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return jsonify([post.to_json() for post in Post.objects])
 
 
 if __name__ == '__main__':
